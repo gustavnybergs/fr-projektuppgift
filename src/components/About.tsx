5bdb7401e59b61a275f67db0e2f094f6abf4d745
Gustav Nyberg <gustavnyberg@MacBook-Pro-som-tillhor-Gustav.local>
2025-02-21 11:15:06 +0100

    clean css structure and remove unused rules

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 const About: React.FC = () => {
     return (
-        <div>
+        <section className="main-content">
             <h1>About Page</h1>
             <p>This is the About page of our app.</p>
-        </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore fugit voluptatibus voluptates officia rerum ipsa ex quod numquam, iste facere ratione, dolorum error esse modi magni voluptatem distinctio tempore libero commodi ea ipsam repellat dicta! Enim sapiente maxime magni? Ratione reprehenderit inventore veritatis eum, esse atque. Quibusdam doloremque a, esse totam exercitationem quas. Eaque neque eius rem aut excepturi. Voluptatibus vel quam qui corporis fuga, animi dolores non consectetur unde molestias labore molestiae dicta nobis sint voluptates provident, ad a praesentium nihil dignissimos dolor! Commodi maiores iusto aliquam officiis sint impedit voluptates minus fuga quo eaque! Omnis dicta minima, voluptas quaerat soluta provident hic a? Voluptas, deserunt voluptates. Porro suscipit assumenda numquam maxime nesciunt nam, nihil ad doloribus quos, sapiente cum, laudantium fugit? Commodi odit voluptatibus voluptatum, atque dicta sit harum consequuntur aspernatur placeat velit voluptates quae qui laudantium similique aut est ea impedit nulla? Officia ipsa id exercitationem labore blanditiis cupiditate! Quod dolores assumenda suscipit! Fugiat quisquam magnam ea facere facilis? Odit non vero saepe dolorum quis sequi fugiat error quod similique maiores incidunt, autem laborum quasi et cumque iure doloremque. Culpa, officiis. Corporis itaque deserunt, aperiam optio quam repellat laboriosam sapiente incidunt ut accusamus voluptatem nam velit veniam.</p>
+        </section>
     );
 };
 
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Home: React.FC = () => {
     return (
-        <div>
+        <section className="main-content">
             <h1>Home Page</h1>
             <p>Welcome to the Home page!</p>
-        </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae voluptas distinctio cupiditate consectetur inventore fugit eveniet dolore veritatis minus voluptatibus consequuntur rem, corrupti, ut alias quisquam? Repellat, at! Voluptates exercitationem modi repellat at tempore cupiditate officia ipsum repudiandae hic recusandae quidem doloribus asperiores dolorum a deleniti iure, est odit deserunt, ea reprehenderit laudantium eius in illo. Ipsam, veniam fugit. Soluta, nemo nesciunt consequuntur earum obcaecati quibusdam ducimus autem voluptatum architecto excepturi, iusto necessitatibus eum iste. Magnam modi tenetur quisquam tempora odit facilis quam neque quaerat inventore explicabo, suscipit cupiditate asperiores consequatur error eum recusandae molestiae eaque mollitia laboriosam dolor minus dolorum maxime! Consequatur corrupti aut distinctio ut quia, sapiente reprehenderit. Tempore tempora culpa vitae, impedit ullam ab natus mollitia illo minima quas, error corrupti iste itaque magni sint est assumenda hic. Dolorum quidem aspernatur fuga optio. Officiis minima assumenda quae! Reiciendis soluta voluptatum sint, quae recusandae quibusdam neque non, placeat nemo vero et sapiente doloribus rem cum velit! Temporibus hic molestiae aperiam ea dolores corrupti ipsam accusantium sequi, alias veniam doloremque fugiat assumenda iste laborum omnis suscipit numquam cupiditate, sunt error beatae expedita dolore nemo! Saepe repellat ex molestias excepturi odit id at, eligendi molestiae veniam. Dolorem totam eos saepe.</p>        
+        </section>
     );
 };
 
